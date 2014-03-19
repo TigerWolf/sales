8be7b27f0a9108b712c4a1a8f74b87c1a3ce2849
Mateus Couto <mateux@gmail.com>
2014-03-19 23:18:21 +1100

    Drying out methods

--- service/cart_calculator.rb
+++ service/cart_calculator.rb
@@ -13,23 +13,27 @@ class CartCalculator
   def total_line_before_tax(item_cart)
     item_cart.quantity * item_cart.price if item_cart
   end
+
+
+  def total_line_after_tax(item)
+    tax_amount = tax_to_be_applyed(item.product)
+    before_tax = total_line_before_tax(item)
+    total = (tax_amount != 0.0) ? apply_tax(tax_amount, before_tax).to_f : before_tax.to_f
+    @amount_sales_total+= total
+    total
+  end
+  def apply_tax(tax_amount, amount_before_tax)
+      total = amount_before_tax + (amount_before_tax * tax_amount)
+      total = round_to(total)
+      @amount_sales_tax+= (total - amount_before_tax)
+      total
+  end
+
    def round_to(amount)
     a = BigDecimal.new(amount.to_s)
     a.mult(1, 2)
     a.add(0, 4)
-  end
-  def total_line_after_tax(item)
-    tax_amount = tax_to_be_applyed(item.product)
-    before_tax = total_line_before_tax(item)
-    if tax_amount != 0.0
-      total = before_tax + (before_tax * tax_amount)
-      total = round_to(total)
-      @amount_sales_tax+= (total-before_tax)
-    else
-      total = before_tax
-    end
-    @amount_sales_total+= total
-    total.to_f
+    a.round(2)
   end
 
   def tax_to_be_applyed(product)
